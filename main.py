d9c0fe85c846417219e8512fd069f2254983a51d
yamahei21python <yamahei21python@gmail.com>
2025-08-13 17:33:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--disable-gpu') # エラー回避のため追加済み
 driver = webdriver.Chrome(options=chrome_options)
 print("WebDriverの準備ができました。")
 
